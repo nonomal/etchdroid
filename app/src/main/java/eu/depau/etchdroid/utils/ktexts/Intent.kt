929893bcd1b7e8781f0901ae0890021a9704ebbe
Davide Depau <davide@depau.eu>
2025-03-23 22:04:55 +0100

    Try adding debug code for safeParcelableExtra NPE

--- app/src/main/java/eu/depau/etchdroid/utils/ktexts/Intent.kt
+++ app/src/main/java/eu/depau/etchdroid/utils/ktexts/Intent.kt
@@ -5,9 +5,26 @@ import android.content.Intent
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import android.os.Build
+import android.util.Log
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
+private fun assertNotNull(value: Any?) {
+    if (value == null) {
+        throw IllegalArgumentException("Value cannot be null")
+    }
+
+    try {
+        val clazz = value::class
+        Log.d("KTexts", "Value is of type $clazz")
+    } catch (e: NullPointerException) {
+        throw IllegalArgumentException("Value cannot be null", e)
+    } catch (e: KotlinNullPointerException) {
+        throw IllegalArgumentException("Value cannot be null", e)
+    }
+}
+
 fun <T> Intent.safeParcelableExtra(key: String, clazz: Class<T>): T? {
+    assertNotNull(clazz)
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getParcelableExtra(key, clazz)
     } else {
