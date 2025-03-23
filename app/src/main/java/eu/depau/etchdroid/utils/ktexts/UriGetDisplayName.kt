f7a0730b7796e72dae3bde77cb1d2bbcc7f420f7
Davide Depau <davide@depau.eu>
2025-03-23 21:45:25 +0100

    Handle failure to obtain uri display name

--- app/src/main/java/eu/depau/etchdroid/utils/ktexts/UriGetDisplayName.kt
+++ app/src/main/java/eu/depau/etchdroid/utils/ktexts/UriGetDisplayName.kt
@@ -3,6 +3,7 @@ package eu.depau.etchdroid.utils.ktexts
 import android.content.Context
 import android.net.Uri
 import android.provider.OpenableColumns
+import eu.depau.etchdroid.plugins.telemetry.Telemetry
 
 const val TAG = "UriGetFileNameExt"
 
@@ -10,19 +11,23 @@ fun Uri.getDisplayName(context: Context): String? {
     var result: String? = null
 
     if (this.scheme == "content") {
-        val cursor = context.contentResolver.query(this, null, null, null, null)
-        cursor.use {
-            if (it != null && it.moveToFirst()) {
-                val colIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                result = it.getString(colIndex)
+        try {
+            val cursor = context.contentResolver.query(this, null, null, null, null)
+            cursor.use {
+                if (it != null && it.moveToFirst()) {
+                    val colIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                    result = it.getString(colIndex)
+                }
             }
+        } catch (e: SecurityException) {
+            Telemetry.captureException("getDisplayName: Failed to query content resolver", e)
         }
     }
     if (result == null) {
         result = this.path
         val cut = result!!.lastIndexOf('/')
         if (cut != -1) {
-            result = result!!.substring(cut + 1)
+            result = result.substring(cut + 1)
         }
     }
 
